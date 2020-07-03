b98a0430f2961ca03d14e5df339269d5b41db138
Kiri Um <kirirotha@gmail.com>
2020-07-03 23:08:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,6 +200,14 @@ def big_shoe_rebounds
     end  
   end  
   puts biggest_shoe_size
+  game_hash.each do |key1, value1|
+    value1[:players].each do |key2, value2|
+      if key2[:shoe] == biggest_shoe_size
+        biggest_shoe_rebounds =  key2[:rebounds]
+      end  
+    end  
+  end
+  puts biggest_shoe_rebounds
 end
 
 
